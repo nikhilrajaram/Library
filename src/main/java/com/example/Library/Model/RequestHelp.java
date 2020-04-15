6ee4e202dc457d0b2a7f2df0a0a84c24434a45d6
TuqaAdel <tual9625@colorado.edu>
2020-04-14 22:00:30 -0600

    librarian style sheet

--- src/main/java/com/example/Library/Model/RequestHelp.java
+++ src/main/java/com/example/Library/Model/RequestHelp.java
@@ -1,9 +1,17 @@
 package com.example.Library.Model;
 
+import com.example.Library.Model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class RequestHelp {
 
-
+    private Integer uid;
     private String content;
+
+    public RequestHelp(String content, User user){
+        this.content = content;
+        this.uid = user.getUid();
+    }
 
     public String getContent() {
         return content;
@@ -12,4 +20,6 @@ public class RequestHelp {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public Integer getUserId(){ return uid; }
 }
