4b43d7ff3d2163ad4211e67044dbe1bfd635192d
nikhilrajaram <nira8877@colorado.edu>
2020-04-16 11:45:06 -0600

    handle post to /requestHelp

--- src/main/java/com/example/Library/Controller/RequestHelpController.java
+++ src/main/java/com/example/Library/Controller/RequestHelpController.java
@@ -3,7 +3,9 @@ package com.example.Library.Controller;
 import com.example.Library.Model.RequestHelp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RequestHelpController {
@@ -14,4 +16,9 @@ public class RequestHelpController {
         return "requestHelp";
     }
 
+    @RequestMapping(value = "/requestHelp", method = RequestMethod.POST)
+    public String handleRequestHelp(@ModelAttribute RequestHelp requestHelp) {
+        return "home";
+        // TODO: reroute successful request to good landing page
+    }
 }
