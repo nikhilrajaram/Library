5a1583d5016b438a642cf79cbd7ce5fe99751e93
TuqaAdel <tual9625@colorado.edu>
2020-04-22 01:10:42 -0600

    Fixing RequestHelpController

--- src/main/java/com/example/Library/Controller/RequestHelpController.java
+++ src/main/java/com/example/Library/Controller/RequestHelpController.java
@@ -5,9 +5,7 @@ import com.example.Library.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RequestHelpController {
@@ -21,14 +19,21 @@ public class RequestHelpController {
     }
 
     @RequestMapping(value = "/requestHelp", method = RequestMethod.POST)
-    public String handleRequestHelp(@ModelAttribute RequestHelp requestHelp) {
+    public String handleRequestHelp(@ModelAttribute User user, Model model) {
+        model.addAttribute("request", new RequestHelp(user, content));
         return "requestSubmitted";
 
     }
 
-    /** This mapping does not work */
     @RequestMapping(value = "/requestSubmitted")
-    public String submitRequest(){
+    public String submitRequest(@ModelAttribute RequestHelp requestHelp){
         return "home";
     }
+
+   @RequestMapping(value = "/home")
+    public String handleSubmittedRequest(){
+        return "home";
+   }
+
+
 }
