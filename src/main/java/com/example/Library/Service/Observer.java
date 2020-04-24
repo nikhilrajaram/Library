2a9fc2f8299ff09f8563c28d987085e3658b12b0
nikhilrajaram <nira8877@colorado.edu>
2020-04-24 15:22:35 -0600

    remove old service package

--- src/main/java/com/example/Library/Service/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.example.Library.Service;
-
-import com.example.Library.Model.User;
-
-public abstract class Observer {
-    protected UserHelpService subject;
-    public void update(LibrarianHelpService observer, String content, User user){};
-}
