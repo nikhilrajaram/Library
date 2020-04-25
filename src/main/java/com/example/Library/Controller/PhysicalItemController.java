6992c536d6a41ee3d293dfb33174e3feef0f2afc
nikhilrajaram <nira8877@colorado.edu>
2020-04-25 15:10:15 -0600

    fix physical check out logic

--- src/main/java/com/example/Library/Controller/PhysicalItemController.java
+++ src/main/java/com/example/Library/Controller/PhysicalItemController.java
@@ -1,8 +1,11 @@
 package com.example.Library.Controller;
 
-import com.example.Library.DAO.ItemDataDAO;
+import com.example.Library.DAO.BookDAOImpl;
 import com.example.Library.DAO.ItemDataDAOImpl;
+import com.example.Library.DAO.MovieDAOImpl;
+import com.example.Library.DAO.RecordDAOImpl;
 import com.example.Library.Model.Item;
+import com.example.Library.Model.Record;
 import com.example.Library.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,7 +15,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Collection;
@@ -20,35 +22,45 @@ import java.util.Collection;
 @Controller
 public class PhysicalItemController {
 
+    @Autowired
+    private ItemDataDAOImpl itemDataDAO;
 
     @Autowired
-    private ItemDataDAO itemDataDAO;
+    private RecordDAOImpl recordDAO;
+
+    @Autowired
+    private BookDAOImpl bookDAO;
+
+    @Autowired
+    private MovieDAOImpl movieDAO;
 
     @RequestMapping(value = "/checkOutPhysicalItem")
-    public String checkOutPhysicalItem(@ModelAttribute Authentication auth, Model model){
+    public String checkOutPhysicalItem(Authentication auth, Model model){
         Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
         if (authorities.contains((new SimpleGrantedAuthority("LIBRARIAN")))) {
             // User is librarian
-            User librarian = new User(auth.getName(), null, true);
+            model.addAttribute("user", new User());
+            model.addAttribute("item", new Item());
             return "checkOutPhysicalItem";
         }
 
-        // if user is not librarian, do not redirect to checkOutPhysicalItem page
+        model.addAttribute("books", bookDAO.getRandomBooks(HomeController.N_CARDS_HOMEPAGE));
+        model.addAttribute("movies", movieDAO.getRandomMovies(HomeController.N_CARDS_HOMEPAGE));
 
-        return "/home";
+        return "home-auth";
     }
 
     @RequestMapping(value = "/checkOutPhysicalItem", method = RequestMethod.POST)
     public String handleCheckOutPhysicalItem(@ModelAttribute User user, @ModelAttribute Item item, Model model){
-        model.addAttribute("user", new User(user.getEmail(), user.getPassword(), user.isEnabled()));
-        model.addAttribute("item", new Item(item.getItemId(),item.getType(), item.getnAvailable(), item.getnCheckedOut(), item.getDigital()));
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("itemID", item.getItemId());
-
-        if(itemDataDAO.isAvailable(item)){
-            item.setnAvailable(item.getnAvailable() - 1);
+        if(itemDataDAO.getnAvailable(item) > 0) {
+            itemDataDAO.checkOutItem(item);
+            Record record = new Record(user, item);
+            model.addAttribute("record", record);
+            recordDAO.insertRecord(record);
             return "item-checked-out";
         }
+
+        model.addAttribute("reason", "This item is out of stock.");
 
         return "failed-check-out";
 
