2bab444e8336ad6070f53f0e6b6cfbf8559b433c
TuqaAdel <tual9625@colorado.edu>
2020-04-26 02:30:44 -0600

    Add Javadoc for HelpRequestDAO

--- src/main/java/com/example/Library/DAO/HelpRequestDAO.java
+++ src/main/java/com/example/Library/DAO/HelpRequestDAO.java
@@ -14,12 +14,32 @@ public interface HelpRequestDAO {
      */
     Boolean addHelpRequest(HelpRequest requestHelp);
 
+    /**
+     * Query help requests for user
+     * @param userEmail user's email associated with help requests
+     * @return List of help requests for user with userEmail
+     */
     List<HelpRequest> getRequestsForUser(String userEmail);
 
+    /**
+     * Query help requests for user
+     * @param user user associated with help requests
+     * @return List of help requests for user
+     */
     List<HelpRequest> getRequestsForUser(User user);
 
+    /**
+     * Query help requests for librarian
+     * @param librarianEmail librarian's email associated with help requests
+     * @return List of help requests for librarian with librarianEmail
+     */
     List<HelpRequest> getRequestsForLibrarian(String librarianEmail);
 
+    /**
+     * Query help requests for librarian
+     * @param librarian librarian associated with help requests
+     * @return List of help requests for librarian
+     */
     List<HelpRequest> getRequestsForLibrarian(User librarian);
 
 }
