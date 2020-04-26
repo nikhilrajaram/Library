b33cce6730464671e82558465409d25d7a28c911
TuqaAdel <tual9625@colorado.edu>
2020-04-26 05:19:38 -0600

    Add javadoc to RecordDAO

--- src/main/java/com/example/Library/DAO/RecordDAO.java
+++ src/main/java/com/example/Library/DAO/RecordDAO.java
@@ -7,9 +7,25 @@ import com.example.Library.Model.User;
 import java.util.List;
 
 public interface RecordDAO {
+
+    /**
+     * If user checks out an item, create a record that has User e-mail and Item ID and insert into records database
+     * @param record to be created
+     * @return if created successfully return true, otherwise return false
+     */
     Boolean insertRecord(Record record);
 
+    /**
+     * Query records for overdue items for user
+     * @param user user that may have overdue items
+     * @return List of records of overdue items by user
+     */
     List<Record> getOverdueItemRecordsByUser(User user);
 
+    /**
+     * Query records by item
+     * @param item item of consideration
+     * @return Record of given item
+     */
     Record getRecordByItem(Item item);
 }
