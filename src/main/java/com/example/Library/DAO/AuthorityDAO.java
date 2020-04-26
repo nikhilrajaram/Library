9d14049f4c90be2ee8cf376b3a52a0de864b9b70
TuqaAdel <tual9625@colorado.edu>
2020-04-26 01:43:12 -0600

    Add Javadoc to AuthorityDAO

--- src/main/java/com/example/Library/DAO/AuthorityDAO.java
+++ src/main/java/com/example/Library/DAO/AuthorityDAO.java
@@ -6,8 +6,15 @@ import java.util.List;
 
 public interface AuthorityDAO {
 
+    /** Get one authority by authority type
+     * @param authorityType type of authorization
+     * @return  Authority */
     Authority getAuthorityByAuthorityType(String authorityType);
 
+    /** Get n authorities by authority type
+     * @param authorityType type of authorization
+     * @param n number of authorities to return
+     * @return  List of n authorities */
     List<Authority> getnAuthoritiesByAuthorityType(String authorityType, Integer n);
 
 }
