fa41ab9c4ba83f5415b7aa02e11e0eb40691c727
TuqaAdel <tual9625@colorado.edu>
2020-04-26 05:09:14 -0600

    Add Javadoc to ObserverRelationDAO

--- src/main/java/com/example/Library/DAO/ObserverRelationDAO.java
+++ src/main/java/com/example/Library/DAO/ObserverRelationDAO.java
@@ -9,11 +9,31 @@ import java.util.List;
 @Repository
 public interface ObserverRelationDAO {
 
+    /**
+     * Query relation between user and librarian
+     * @param userEmail user's email of consideration
+     * @return List of Observer relation
+     */
     List<ObserverRelation> getObserverRelationsByObservable(String userEmail);
 
+    /**
+     * Query relation between user and librarian
+     * @param user user of consideration
+     * @return List of Observer relation
+     */
     List<ObserverRelation> getObserverRelationsByObservable(User user);
 
+    /**
+     * Add Observer relation to database of observer_relations
+     * @param relation relation to be added
+     * @return if added successfully return true, otherwise return false
+     */
     Boolean addObserverRelation(ObserverRelation relation);
 
+    /**
+     * Remove Observer relation from database of observer_relations
+     * @param relation to be removed
+     * @return if removed successfully return true, otherwise return false
+     */
     Boolean removeObserverRelation(ObserverRelation relation);
 }
